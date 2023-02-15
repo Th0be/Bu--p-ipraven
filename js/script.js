908313f108c0f9e5e5dca0f0720a25dd583403b4
Th0be <navrat.tobias@gmail.com>
2023-02-15 21:30:06 +0000

    Google analytics changes, formation

--- js/script.js
+++ js/script.js
@@ -3,26 +3,30 @@ document.querySelector("#menuMobile").addEventListener("click", mobileMenu);
 window.addEventListener("resize", windowResize);
 
 //ZOBRAZOVÁNÍ SPRÁVNÉHO MENU PŘI ZMĚNĚ VELIKOSTI OKNA
-function windowResize(){
-    if(document.body.scrollWidth>=850){
-        document.getElementById("menu").style.transition = "opacity 0s";
-        document.getElementById("menu").style.opacity = "1";
-        document.getElementById("menu").style.display = "block";
-        document.getElementById("menuMobileImage").style.transform = "rotate(0deg)";
-    }
-    else if(document.body.scrollWidth<850){
-        document.getElementById("menu").style.transition = "opacity 0s";
-        document.getElementById("menu").style.opacity = "0";
-    }
+function windowResize() {
+  if (document.body.scrollWidth >= 850) {
+    document.getElementById("menu").style.transition = "opacity 0s";
+    document.getElementById("menu").style.opacity = "1";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menuMobileImage").style.transform = "rotate(0deg)";
+  } else if (document.body.scrollWidth < 850) {
+    document.getElementById("menu").style.transition = "opacity 0s";
+    document.getElementById("menu").style.opacity = "0";
+  }
 }
 //MOBILNÍ MENU
-function mobileMenu(){
-    document.getElementById("menuMobileImage").style.transition = "transform 0.5s";
-    if(document.getElementById("menuMobileImage").style.transform == "rotate(270deg)"){
-        document.getElementById("menuMobileImage").style.transform = "rotate(0deg)";
-        document.getElementById("menu").style.display = "none";
-    }else{
-        document.getElementById("menuMobileImage").style.transform = "rotate(270deg)";
-        document.getElementById("menu").style.display = "block";
-    }
+function mobileMenu() {
+  document.getElementById("menuMobileImage").style.transition =
+    "transform 0.5s";
+  if (
+    document.getElementById("menuMobileImage").style.transform ==
+    "rotate(270deg)"
+  ) {
+    document.getElementById("menuMobileImage").style.transform = "rotate(0deg)";
+    document.getElementById("menu").style.display = "none";
+  } else {
+    document.getElementById("menuMobileImage").style.transform =
+      "rotate(270deg)";
+    document.getElementById("menu").style.display = "block";
+  }
 }
